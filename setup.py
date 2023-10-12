33adef91c3d2aa184f722ac0be718ce1824886dd
Fabian Hoppe <112093564+mrfh92@users.noreply.github.com>
2023-10-12 12:56:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering",
@@ -34,7 +35,7 @@ setup(
     install_requires=[
         "mpi4py>=3.0.0",
         "numpy>=1.20.0",
-        "torch>=1.8.0, <2.0.2",
+        "torch>=1.8.0",
         "scipy>=0.14.0",
         "pillow>=6.0.0",
         "torchvision>=0.8.0",
